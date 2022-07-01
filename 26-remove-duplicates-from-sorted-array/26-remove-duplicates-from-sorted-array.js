bb0150ed6dcb2105d9aa7f542380e3fabad04138
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-01 11:32:24 -0400

    Time: 110 ms (60.31%), Space: 44.4 MB (85.04%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 0;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] === nums[k]){
+            continue
+        } else {
+            k++
+            nums[k] = nums[i]
+        }
+    }
+    k++
+    return k
+};
