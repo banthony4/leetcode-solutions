2f9e37f18fc924f0333fc8ee3139402da6fe4ffe
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-01 11:43:05 -0400

    Time: 91 ms (41.38%), Space: 42.4 MB (25.71%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(needle.length === 0 || needle === ''){
+      return 0  
+    } 
+    for(let i = 0; i < haystack.length; i++){
+        if( haystack.substring(i,i+needle.length) === needle){
+            return i
+        }
+    }
+    return -1
+};
