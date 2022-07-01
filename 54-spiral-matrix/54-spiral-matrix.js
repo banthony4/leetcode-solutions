c3623d56f89f0f35401091748b366730df24b1bb
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-01 11:46:37 -0400

    Time: 126 ms (5.22%), Space: 41.9 MB (58.74%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    const res = []
+  while(matrix.length){
+    const first = matrix.shift()
+    res.push(...first)
+    for(const m of matrix){
+      let val = m.pop()
+      if(val)
+        res.push(val)
+        m.reverse()   
+    }
+    matrix.reverse()
+  }
+  return res
+
+};
