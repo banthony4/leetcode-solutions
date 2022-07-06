13177e1820e2b8decd5099efa4c8ee9ba0cbd58f
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-06 15:53:43 -0400

    Time: 445 ms (39.25%), Space: 72.2 MB (19.74%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(T) {
+    let stack = [];
+    let result = new Array(T.length).fill(0)
+    for(let i in T){
+        while(stack.length && T[i] > T[stack[stack.length-1]]){
+            let temp = stack.pop();
+            result[temp] = i - temp
+        }
+        stack.push(i)
+    }
+    
+    return result;
+};
