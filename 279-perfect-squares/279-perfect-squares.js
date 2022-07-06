ddb319c432952d2d21f3ddec5228b78855f862b3
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-06 14:43:57 -0400

    Time: 201 ms (78.15%), Space: 52.2 MB (11.22%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.js
+++ 279-perfect-squares/279-perfect-squares.js
@@ -6,13 +6,18 @@ var numSquares = function(n) {
     let q = [n]
     const visited = new Set()
     let steps = 1;
+    let squareNums = []
+    for(let i = 1; i**2 <= n; i++){
+        squareNums.push(i**2)
+    }
+    
     
     while(q.length){
         const size = q.length;
         for (let i = 0; i < size; i++) {
           const cur = q.shift();
-          for (let j = 1; j ** 2 <= cur; j++) {
-            const diff = cur - j ** 2;
+          for (let j = 0; j < squareNums.length; j++) {
+            const diff = cur - squareNums[j];
             if (diff === 0) {
               return steps;
             }
