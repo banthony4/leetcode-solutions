5c97c248c217d38e813c2f29ccf3ebff0930181a
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-06 16:59:48 -0400

    Time: 100 ms (79.88%), Space: 45.3 MB (56.62%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,47 +3,25 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let count = 0;
-    const WATER = '0';
-    const LAND = '1';
-    const DIRECTIONS = [
-        [0, 1],
-        [1, 0],
-        [0, -1],
-        [-1, 0],
-    ];
+    let islandCount = 0;
     
-    function bfs(grid, r, c ) {
-        let queue = [[r, c]];
-        grid[r][c] = WATER;
-
-        while (queue.length) {
-            let size = queue.length;
-            for (let i = 0; i < size; i++) {
-                let [row, col] = queue.pop();
-
-                for (let [x, y] of DIRECTIONS) {
-                    let iRow = row + x;
-                    let iCol = col + y;
-
-                    if (iRow < 0 || iRow >= grid.length || iCol < 0 || iCol >= grid[0].length || grid[iRow][iCol] !== LAND) {
-                        continue;
-                    }
-                    grid[iRow][iCol] = WATER;
-                    queue.unshift([iRow, iCol]);
-                }
+    function dfs(r, c) {
+        if(grid[r] === undefined || grid[r][c] === undefined || grid[r][c] === '0') return;
+        grid[r][c] = '0';
+        dfs(r+1, c);
+        dfs(r-1, c);
+        dfs(r, c+1);
+        dfs(r, c-1);
+    }
+    
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[r].length; c++) {
+            if (grid[r][c] === '1') {
+                islandCount++;
+                dfs(r, c);
             }
         }
     }
     
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] === LAND) {
-                count++;
-                bfs(grid, i, j);
-            }
-        }
-    }
-    return count;
-    
+    return islandCount;
 };
