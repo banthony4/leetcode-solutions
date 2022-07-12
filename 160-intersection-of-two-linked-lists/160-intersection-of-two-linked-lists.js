334d590e533f067b1296a0d38cc443598853e0b4
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-12 14:00:40 -0400

    Time: 98 ms (90.11%), Space: 50.1 MB (48.06%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if(!headA || !headB) return null;
+    let pA = headA, pB = headB;
+    
+    while(pA !== pB){
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
+    }
+    return pA;
+};
