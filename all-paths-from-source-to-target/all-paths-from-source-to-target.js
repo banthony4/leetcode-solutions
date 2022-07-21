d82b285928f0ca3484998987da7f78271d589b6b
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-07-21 09:52:12 -0400

    Time: 95 ms, Memory: 51.1 MB - LeetHub

---
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    const target = graph.length - 1;
+    const res = [];
+    
+    const DFS = (node,path) => {
+        path.push(node);
+
+        if(node === target) { 
+            res.push(path);
+            return;
+        }
+            
+        for(let edge of graph[node]) {
+            DFS(edge,[...path]);
+        }
+    }
+    DFS(0,[]);
+    
+    return res;
+}
