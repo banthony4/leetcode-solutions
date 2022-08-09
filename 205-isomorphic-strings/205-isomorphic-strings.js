7bede8ca21b7b68d1a0d3caebedb4e31b722205a
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-08-09 13:15:18 -0400

    Time: 102 ms (65.94%), Space: 42.7 MB (72.43%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let map = new Map(), set = new Set()
+    
+    for(let i = 0; i < s.length; i++){
+        let sChar = s[i], tChar = t[i]
+        if(map.has(sChar)){
+            if(map.get(sChar) !== tChar)return false
+        }else{
+            if(set.has(tChar))return false
+            map.set(sChar, tChar)
+            set.add(tChar)
+        }
+    }
+    return true
+}
