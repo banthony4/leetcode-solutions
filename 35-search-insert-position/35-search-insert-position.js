1f09d2b2cd05eb8f7d9afde134cd14213cd4e7ca
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-08-10 10:42:57 -0400

    Time: 88 ms (51.28%), Space: 42.3 MB (35.67%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -10,10 +10,10 @@ var searchInsert = function(nums, target) {
         let mid = Math.floor((hi+lo)/2)
         if(nums[mid] === target) return mid;
         
-        if(nums[mid] < target){
-            lo = mid + 1
-        } else {
+        if(nums[mid] > target){
             hi = mid - 1
+        } else {
+            lo = mid + 1
         }
     }
     return lo
