56200caa6765bcf63cf91f293288e613df9d6b3b
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-08-10 10:41:58 -0400

    Time: 86 ms (55.37%), Space: 42.2 MB (47.14%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let hi = nums.length-1, lo = 0;
+    
+    while(lo <= hi){
+        let mid = Math.floor((hi+lo)/2)
+        if(nums[mid] === target) return mid;
+        
+        if(nums[mid] < target){
+            lo = mid + 1
+        } else {
+            hi = mid - 1
+        }
+    }
+    return lo
+};
