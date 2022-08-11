338fe690309e8097ab6acdbae7da71c6343b2360
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-08-11 09:22:42 -0400

    Time: 103 ms (66.24%), Space: 45.3 MB (37.82%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -14,7 +14,7 @@ var detectCycle = function(head) {
     let slow = head;
     let fast = head;
     
-    while(fast !== null && fast.next !== null){
+    while(fast && fast.next){
         slow = slow.next;
         fast = fast.next.next;
         if(slow === fast){
