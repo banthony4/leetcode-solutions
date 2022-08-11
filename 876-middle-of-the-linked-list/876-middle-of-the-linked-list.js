e39c6fcebbb90d4993e55d3c4eb7837fad19e712
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-08-11 09:20:45 -0400

    Time: 79 ms (66.16%), Space: 42 MB (52.98%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let p1 = head, p2 = head;
+    
+    while(p2.next){
+        if(!p2.next.next) return p1.next;
+        p1 = p1.next;
+        p2 = p2.next.next;
+    }
+    return p1;
+};
