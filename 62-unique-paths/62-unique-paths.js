215ca85a357b18e46f07cab62d2d89d50218fc87
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-09-27 12:12:52 -0400

    Time: 119 ms (10.48%), Space: 42.2 MB (60.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -5,11 +5,11 @@
  */
 var uniquePaths = function(m, n) {
     let currentRow = new Array(n);
-    // Assigning a 1 to matrix[0][0] is simply a shortcut that skips some later computation
-    // as matrix[i][0] will never change in this iterative process
+    
     for (let i = 0; i < n; i++) {
         currentRow[i] = 1;
     }
+    
     for (let row = 1; row < m; row++) {
         for (let i = 1; i < n; i++) {
             currentRow[i] += currentRow[i - 1];
