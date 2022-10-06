a3b5ea8cc5f8618e7659b616f69c589d72845291
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-10-06 10:38:44 -0400

    Time: 107 ms, Memory: 211 MB - LeetHub

---
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def find_numbers(nums)
+    count = 0
+    
+    nums.each do |num|
+        if num.to_s.length % 2 == 0
+            count += 1
+        end
+    end
+    count
+end
