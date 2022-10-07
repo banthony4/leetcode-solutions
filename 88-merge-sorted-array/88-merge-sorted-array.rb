e51b0e739af817cbdc8259e24485f1d46c1340ae
Ben Anthony <99452309+banthony4@users.noreply.github.com>
2022-10-07 08:32:30 -0400

    Time: 158 ms (36.23%), Space: 211 MB (81.64%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.rb
@@ -0,0 +1,21 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+    
+    while m > 0 and n > 0
+       if nums1[m-1] > nums2[n-1]
+           nums1[m+n-1] = nums1[m-1]
+           m -= 1
+       else
+           nums1[m+n-1] = nums2[n-1]
+           n -= 1
+       end
+    end
+
+    if n > 0
+           nums1[0...n] = nums2[0...n] 
+    end
+end
